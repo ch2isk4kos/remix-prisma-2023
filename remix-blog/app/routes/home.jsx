e72dfab5791eb0cfc66c2641d16223103d5904ed
ch2isk4kos <ch2isk4kos@gmail.com>
2023-03-19 18:00:23 -0400

    add relative path link(s)

--- remix-blog/app/routes/home.jsx
+++ remix-blog/app/routes/home.jsx
@@ -1,10 +1,7 @@
-import { Link } from "@remix-run/react";
-
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <Link to="/home">Go Home</Link>
+      <h1>Welcome Home!</h1>
     </div>
   );
 }
