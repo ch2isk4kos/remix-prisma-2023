5fcdba103f097623889e168411c2ce1fa50c7505
ch2isk4kos <ch2isk4kos@gmail.com>
2023-03-20 23:15:00 -0400

    add error boundary function

--- remix-blog/app/root.jsx
+++ remix-blog/app/root.jsx
@@ -1,5 +1,5 @@
-import Document from "./components/layouts/Document";
-import Layout from "./components/layouts/Layout";
+import Document from "./components/layouts/document";
+import Layout from "./components/layouts/index";
 import { Outlet } from "@remix-run/react";
 import styles from "./styles/global.css";
 
@@ -21,4 +21,16 @@ export default function App() {
 
 export function links() {
   return [{ rel: "stylesheet", href: styles }];
+}
+
+export function ErrorBoundary({ error }) {
+  console.log("error:", error);
+  return (
+    <Document>
+      <Layout>
+        <h1>Error</h1>
+        <p>{error.message}</p>
+      </Layout>
+    </Document>
+  );
 }
