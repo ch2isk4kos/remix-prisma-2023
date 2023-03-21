fce378be6adcd794b1f925a27b0ecc732959253d
ch2isk4kos <ch2isk4kos@gmail.com>
2023-03-20 23:16:10 -0400

    rename component

--- remix-blog/app/routes/index.jsx
+++ remix-blog/app/routes/index.jsx
@@ -1,10 +1,10 @@
 import { Link } from "@remix-run/react";
 
-export default function Index() {
+export default function Landing() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1>Welcome to Remix</h1>
-      <Link to="/home">Go Home</Link>
+      <Link to="/home">Enter</Link>
     </div>
   );
 }
