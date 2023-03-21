ead0859cbaab0af3212749ee8f2914bedbcde470
ch2isk4kos <ch2isk4kos@gmail.com>
2023-03-20 23:15:33 -0400

    rename component

--- remix-blog/app/routes/home.jsx
+++ remix-blog/app/routes/home.jsx
@@ -1,4 +1,4 @@
-export default function Index() {
+export default function Home() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1>Welcome Home!</h1>
