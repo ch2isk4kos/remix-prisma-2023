00d2b46d5ec57cdfa56ed05f09da57b48d030332
ch2isk4kos <ch2isk4kos@gmail.com>
2023-03-20 23:19:34 -0400

    add async action function to handle on form submit

--- remix-blog/app/routes/posts/new.jsx
+++ remix-blog/app/routes/posts/new.jsx
@@ -1,18 +1,50 @@
+import { Form, Link } from "@remix-run/react";
+import { redirect } from "@remix-run/node";
+
+export const action = async ({ request }) => {
+  // console.log("request:", request);
+
+  const form = await request.formData();
+  console.log("form:", form);
+
+  const title = form.get("title");
+  const content = form.get("content");
+
+  const fields = { title, content };
+  console.log("fields:", fields);
+
+  // @TODO: submit to database
+
+  return redirect("/posts");
+};
+
 export default function NewPost() {
   return (
-    <form method="post" className="NewPost">
-      <h2>Create a Post</h2>
-      <p>
-        <label htmlFor="title">Title</label>
-        <input type="text" id="title" name="title" required />
-      </p>
-      <p>
-        <label htmlFor="content">Content</label>
-        <textarea type="text" id="content" name="content" rows="5" required />
-      </p>
-      <div className="form-actions">
-        <button>Add Post</button>
+    <>
+      {/* HEADER */}
+      <div>
+        <h2>Create a Post</h2>
+        <Link to="/posts">Go Back</Link>
       </div>
-    </form>
+      {/* FORM */}
+      <div>
+        <Form method="post">
+          {/* title */}
+          <div>
+            <label htmlFor="title">Title</label>
+            <input type="text" id="title" name="title" required />
+          </div>
+          {/* content */}
+          <div>
+            <label htmlFor="content">Content</label>
+            <textarea id="content" name="content" required />
+          </div>
+          {/* submit button */}
+          <div>
+            <button type="submit">Add Post</button>
+          </div>
+        </Form>
+      </div>
+    </>
   );
 }
