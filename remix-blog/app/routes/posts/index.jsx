550a237cda9fe601adabf9867fad3266c2e8bed0
ch2isk4kos <ch2isk4kos@gmail.com>
2023-03-21 20:42:06 -0400

    remove comment(s)

--- remix-blog/app/routes/posts/index.jsx
+++ remix-blog/app/routes/posts/index.jsx
@@ -1,14 +1,18 @@
 import { Link } from "@remix-run/react";
 import { useLoaderData } from "react-router";
+import { db } from "~/utils/db.server.ts";
 import styles from "../../styles/posts/index.css";
 
 export const loader = () => {
   const data = {
-    posts: [
-      { id: "1", title: "Post 1", body: "First ever post." },
-      { id: "2", title: "Post 2", body: "This is the second post." },
-      { id: "3", title: "Post 3", body: "And the third." },
-    ],
+    // posts: [
+    //   { id: "1", title: "Post 1", content: "First ever post." },
+    //   { id: "2", title: "Post 2", content: "This is the second post." },
+    //   { id: "3", title: "Post 3", content: "And the third." },
+    // ],
+    posts: db.post.findMany,
+    take: 20,
+    select: { id: true, title: true, content: true },
   };
   return data;
 };
@@ -25,13 +29,14 @@ export default function Posts() {
         </Link>
       </div>
       <ul className="posts-list">
-        {posts.map((post) => (
-          <li key={post.id}>
-            <Link to={post.id}>
-              <h3>{post.title}</h3>
-            </Link>
-          </li>
-        ))}
+        {posts &&
+          posts.map((post) => (
+            <li key={post.id}>
+              <Link to={post.id}>
+                <h3>{post.title}</h3>
+              </Link>
+            </li>
+          ))}
       </ul>
     </div>
   );
