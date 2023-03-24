b0ec7fb0c9725c2dce8608d5ab3be393278bb964
ch2isk4kos <ch2isk4kos@gmail.com>
2023-03-23 20:08:43 -0400

    remove commented code

--- remix-blog/app/routes/posts/index.jsx
+++ remix-blog/app/routes/posts/index.jsx
@@ -5,11 +5,6 @@ import styles from "../../styles/posts/index.css";
 
 export const loader = async () => {
   const data = {
-    // posts: [
-    //   { id: "1", title: "Post 1", content: "First ever post." },
-    //   { id: "2", title: "Post 2", content: "This is the second post." },
-    //   { id: "3", title: "Post 3", content: "And the third." },
-    // ],
     posts: await db.post.findMany(),
     // select: { id: true, title: true, content: true },
   };
