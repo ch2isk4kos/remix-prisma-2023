ffddaf873bebfc7fc017df3d0deacbcc0b0ca568
ch2isk4kos <ch2isk4kos@gmail.com>
2023-03-27 15:37:06 -0400

    declare varibale to capture post query

--- remix-blog/app/routes/posts/$postId.jsx
+++ remix-blog/app/routes/posts/$postId.jsx
@@ -2,7 +2,9 @@ import { useLoaderData } from "@remix-run/react";
 import { db } from "~/utils/db.server";
 
 export const loader = async ({ params }) => {
-  // const post = await
+  const post = await db.post.findUnique({
+    where: { id: params.postId },
+  });
 };
 
 export default function Post() {
