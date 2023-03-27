fef4bc17b1fe6fe95673b8bb44a9594796282906
ch2isk4kos <ch2isk4kos@gmail.com>
2023-03-27 15:32:16 -0400

    import useLoaderData native hook

--- remix-blog/app/routes/posts/$postId.jsx
+++ remix-blog/app/routes/posts/$postId.jsx
@@ -1,11 +1,9 @@
-import { useParams } from "@remix-run/react";
+import { useLoaderData } from "@remix-run/react";
 
 export default function Post() {
-  const params = useParams();
-
   return (
     <div className="Post">
-      <h2>Post {params.postId}</h2>
+      <h2>Post {"Post"}</h2>
     </div>
   );
 }
