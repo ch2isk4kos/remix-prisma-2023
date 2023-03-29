653735e4b8a21e51104c3c87b3d2577315008747
ch2isk4kos <ch2isk4kos@gmail.com>
2023-03-29 12:13:18 -0400

    add conditional statement to validate title data type/length

--- remix-blog/app/utils/validations/post.js
+++ remix-blog/app/utils/validations/post.js
@@ -1,5 +1,6 @@
 export const validateTitle = ({ title }) => {
-  //
+  if (typeof title !== "string" || title.length < 3)
+    return "Title needs to be at least 3 characters long";
 };
 
 export const validateContent = ({ content }) => {
