5811f6521aa6092968e8f777767fc7dfe970d1e4
ch2isk4kos <ch2isk4kos@gmail.com>
2023-03-29 12:42:53 -0400

    fix: remove prop destucture from parameter(s)

--- remix-blog/app/utils/validations/post.js
+++ remix-blog/app/utils/validations/post.js
@@ -1,9 +1,9 @@
-export const validateTitle = ({ title }) => {
+export const validateTitle = (title) => {
   if (typeof title !== "string" || title.length < 3)
     return "Title needs to be at least 3 characters long";
 };
 
-export const validateContent = ({ content }) => {
+export const validateContent = (content) => {
   if (typeof content !== "string" || content.length < 10)
     return "Body must be at least 10 characters long";
 };
